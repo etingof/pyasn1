0f64f4b99723a00cba955936deeb8b60e3b3e77a
elie <elie>
2006-04-24 14:26:37 +0000

    set default value (0) to EndOfOctets

--- pyasn1/codec/ber/eoo.py
+++ pyasn1/codec/ber/eoo.py
@@ -1,6 +1,7 @@
 from pyasn1.type import base, tag
 
 class EndOfOctets(base.AbstractSimpleAsn1Item):
+    defaultValue = 0
     tagSet = tag.initTagSet(
         tag.Tag(tag.tagClassUniversal, tag.tagFormatSimple, 0x00)
         )
