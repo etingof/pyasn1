7b5906c7aac46aea04378a77c00d4a5fa363c7c1
elie <elie>
2009-08-22 18:09:16 +0000

    API versioning mechanics retired (pyasn1.v1 -> pyasn1)

--- setup.py
+++ setup.py
@@ -19,17 +19,16 @@ except ImportError:
     from distutils.core import setup
 
 setup(name="pyasn1",
-      version="0.0.9a",
+      version="0.0.10a",
       description="ASN.1 library for Python",
       author="Ilya Etingof",
       author_email="ilya@glas.net ",
       url="http://sourceforge.net/projects/pyasn1/",
       packages = [ 'pyasn1',
-                   'pyasn1.v1',
-                   'pyasn1.v1.type',
-                   'pyasn1.v1.codec',
-                   'pyasn1.v1.codec.ber',
-                   'pyasn1.v1.codec.cer',
-                   'pyasn1.v1.codec.der' ],
+                   'pyasn1.type',
+                   'pyasn1.codec',
+                   'pyasn1.codec.ber',
+                   'pyasn1.codec.cer',
+                   'pyasn1.codec.der' ],
       license="BSD"
       )
