ae6d7001198b341dcb80cacc8697215055524b48
elie <elie>
2011-01-22 11:50:29 +0000

    updated

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ except ImportError:
 
 params.update( {
     'name': 'pyasn1',
-    'version': '0.0.12a',
+    'version': '0.0.13a',
     'description': 'ASN.1 types and codecs',
     'author': 'Ilya Etingof',
     'author_email': 'ilya@glas.net',
