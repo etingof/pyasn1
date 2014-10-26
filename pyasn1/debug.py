2c6afa67ba21acec7e6488e03317b1705f3ddfd1
elie <elie>
2014-10-26 16:27:15 +0000

    built-in debugging is now based on Python logging module

--- pyasn1/debug.py
+++ pyasn1/debug.py
@@ -1,4 +1,5 @@
-import sys
+import time
+import logging
 from pyasn1.compat.octets import octs2ints
 from pyasn1 import error
 from pyasn1 import __version__
@@ -14,13 +15,33 @@ flagMap = {
     'all': flagAll
     }
 
+class Printer:
+    def __init__(self, logger=None, handler=None, formatter=None):
+        if logger is None:
+            logger = logging.getLogger('pyasn1')
+        logger.setLevel(logging.DEBUG)
+        if handler is None:
+            handler = logging.StreamHandler()
+        if formatter is None:
+            formatter = logging.Formatter('%(name)s: %(message)s')
+        handler.setFormatter(formatter)
+        handler.setLevel(logging.DEBUG)
+        logger.addHandler(handler)
+        self.__logger = logger
+
+    def __call__(self, msg): self.__logger.debug(msg)
+    def __str__(self): return '<python built-in logging>'
+
 class Debug:
-    defaultPrinter = sys.stderr and sys.stderr.write or None
-    def __init__(self, *flags):
+    defaultPrinter = None
+    def __init__(self, *flags, **options):
         self._flags = flagNone
-        if not self.defaultPrinter:
-            raise error.PyAsn1Error('Null debug writer specified')
-        self._printer = self.defaultPrinter
+        if options.get('printer') is not None:
+            self._printer = options.get('printer')
+        elif self.defaultPrinter is not None:
+            self._printer = self.defaultPrinter
+        else:
+            self._printer = Printer()
         self('running pyasn1 version %s' % __version__)
         for f in flags:
             if f not in flagMap:
@@ -32,13 +53,17 @@ class Debug:
         return 'logger %s, flags %x' % (self._printer, self._flags)
     
     def __call__(self, msg):
-        self._printer('DBG: %s\n' % msg)
+        self._printer('[%s]: %s' % (self.timestamp(), msg))
 
     def __and__(self, flag):
         return self._flags & flag
 
     def __rand__(self, flag):
         return flag & self._flags
+
+    def timestamp(self):
+        return time.strftime('%H:%M:%S', time.localtime()) + \
+               '.%.3d' % int((time.time() % 1) * 1000)
 
 logger = 0
 
